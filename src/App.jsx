11a92881a7f1304e21f6c290eaad0ee21bbadddd
mutasemAlsaifi <mutasemalsaifi608@gmail.com>
2024-11-21 09:31:57 +0200

    update some logical mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from '/src/components/Header'
 import ListGroups from '/src/components/ListGroups'
+import {removeDuplicates } from './RemoveDuplicates';
+import { SumandAvg } from './SumandAvg';
 const App = () => {
   let items = ["red", "orange", "yellow"];
+  removeDuplicates(["apple", "banana", "apple", "orange", "banana", "grape", "apple", "orange", "banana", "grape"])
+  SumandAvg([10,20,30,40,50]);
   return (
     <div>
       <Header heading='PRODUCTS'/>
--- src/RemoveDuplicates.js
+++ src/RemoveDuplicates.js
@@ -1,22 +1,25 @@
 //Task 3
-let my_array = ["apple", "banana", "apple", "orange", "banana", "grape", "apple", "orange", "banana", "grape"];
+export  function removeDuplicates(my_array){
+    
 
 
-let n = my_array.length
-
-
-for (let i = 0; i < n - 1; i++) {   
-    for (let j = i + 1; j < n; j++) {
-        
-        if (my_array[i] === my_array[j]) {
-            my_array.splice(j, 1);
-
+    let n = my_array.length
+    
+    
+    for (let i = 0; i < n - 1; i++) {   
+        for (let j = i + 1; j < n; j++) {
+            
+            if (my_array[i] === my_array[j]) {
+                my_array.splice(j, 1);
+    
+            }
         }
+    
     }
-
+    
+    console.log("Unique array:", my_array)
 }
 
 
-console.log("Unique array:", my_array)
 
 
